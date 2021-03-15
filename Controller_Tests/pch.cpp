5152dd51c4d487d779bbcaa5d41edb437e5f00d5
ilan hartskeerl <ilan@hartskeerl.nl>
2021-03-15 14:17:11 +0100

    Added copyright claim

--- Controller_Tests/pch.cpp
+++ Controller_Tests/pch.cpp
@@ -1,3 +1,9 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
 //
 // pch.cpp
 // Include the standard header and generate the precompiled header.

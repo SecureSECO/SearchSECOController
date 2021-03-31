ff014bb89564221dcae1ba235fb0bbe5b23993be
Jochem Bloem <j.bloem1@uu.nl>
2021-03-31 09:57:30 +0200

    Added licenses to the new files

--- Flags.h
+++ Flags.h
@@ -1,3 +1,10 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
+
 #pragma once
 
 #include "Utils.h"

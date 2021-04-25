eefc95865e1bd52b4c62b0d43806c3d1da0de4c9
Jochem Bloem <j.bloem1@uu.nl>
2021-04-25 14:50:01 +0200

    Added copyright header

--- src/regex.cpp
+++ src/regex.cpp
@@ -1,3 +1,9 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
 #include "regex.h"
 
 bool regex::validateSyntax(std::string callStr, std::tuple<std::string, std::string, std::string> &result)

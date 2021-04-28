5bb020655d979b664ec03558f833eb513657b3c2
IzakAlva <izakhaak@gmail.com>
2021-04-28 15:12:12 +0200

    init

---
+++ Controller_Tests/Integration_tests.cpp
@@ -0,0 +1,27 @@
+﻿/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+� Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
+#include "pch.h"
+
+TEST(IntegrationTest, Crawler)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(IntegrationTest, Spider)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(IntegrationTest, Parser)
+{
+    EXPECT_TRUE(true);
+}
+
+TEST(IntegrationTest, DatabaseAPI)
+{
+    EXPECT_TRUE(true);
+}

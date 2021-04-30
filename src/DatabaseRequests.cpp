d52f0e441f449e2098ca374beca21e4566971a31
ilan hartskeerl <ilan@hartskeerl.nl>
2021-04-30 12:42:06 +0200

    Changed spaces into tabs

--- src/DatabaseRequests.cpp
+++ src/DatabaseRequests.cpp
@@ -18,53 +18,53 @@ Utrecht University within the Software Project course.
 
 std::string DatabaseRequests::uploadHashes(std::vector<HashData> hashes, ProjectMetaData metaData, AuthorData authorData)
 {
-    int dataSize = 0;
-    char* rawData = NetworkUtils::getAllDataFromHashes(hashes, dataSize, metaData.getAsHeader(), authorData);
+	int dataSize = 0;
+	char* rawData = NetworkUtils::getAllDataFromHashes(hashes, dataSize, metaData.getAsHeader(), authorData);
 
-    return execRequest(DATABASE_UPLOAD_REQUEST, rawData, dataSize);
+	return execRequest(DATABASE_UPLOAD_REQUEST, rawData, dataSize);
 }
 
 std::string DatabaseRequests::findMatches(std::vector<HashData> hashes)
 {
-    int dataSize = 0;
-    char* rawData = NetworkUtils::getHashDataFromHashes(hashes, dataSize);
+	int dataSize = 0;
+	char* rawData = NetworkUtils::getHashDataFromHashes(hashes, dataSize);
 
-    return execRequest(DATABASE_CHECK_REQUEST, rawData, dataSize);
+	return execRequest(DATABASE_CHECK_REQUEST, rawData, dataSize);
 }
 
 std::string DatabaseRequests::checkUploadHashes(std::vector<HashData> hashes, ProjectMetaData metaData, AuthorData authorData)
 {
-    int dataSize = 0;
-    char* rawData = NetworkUtils::getAllDataFromHashes(hashes, dataSize, metaData.getAsHeader(), authorData);
+	int dataSize = 0;
+	char* rawData = NetworkUtils::getAllDataFromHashes(hashes, dataSize, metaData.getAsHeader(), authorData);
 
-    return execRequest(DATABASE_CHECK_UPLOAD_REQUEST, rawData, dataSize);
+	return execRequest(DATABASE_CHECK_UPLOAD_REQUEST, rawData, dataSize);
 }
 
 std::string DatabaseRequests::execRequest(std::string request, char* rawData, int dataSize)
 {
-    // First start the connection.
-    NetworkHandler* networkHandler = startConnection();
+	// First start the connection.
+	NetworkHandler* networkHandler = startConnection();
 
-    // Then send the header (what request we are doing and how much data we are sending).
-    std::string requestType = request + std::to_string(dataSize) + "\n";
-    networkHandler->sendData(requestType);
+	// Then send the header (what request we are doing and how much data we are sending).
+	std::string requestType = request + std::to_string(dataSize) + "\n";
+	networkHandler->sendData(requestType);
 
-    // After that we send the data.
-    networkHandler->sendData(rawData, dataSize);
+	// After that we send the data.
+	networkHandler->sendData(rawData, dataSize);
 
-    // Then we wait for the output that the API gives us.
-    std::string output = networkHandler->receiveData();
+	// Then we wait for the output that the API gives us.
+	std::string output = networkHandler->receiveData();
 
-    // Deleting the data we send and closing the connection.
-    delete[] rawData;
-    delete networkHandler;
+	// Deleting the data we send and closing the connection.
+	delete[] rawData;
+	delete networkHandler;
 
-    return output;
+	return output;
 }
 
 NetworkHandler* DatabaseRequests::startConnection()
 {
-    NetworkHandler* nh = NetworkHandler::createHandler();
-    nh->openConnection(DATABASE_API_IP, DATABASE_API_PORT);
-    return nh;
+	NetworkHandler* nh = NetworkHandler::createHandler();
+	nh->openConnection(DATABASE_API_IP, DATABASE_API_PORT);
+	return nh;
 }

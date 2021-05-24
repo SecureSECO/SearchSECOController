d90802a44578a73e06ceb4cabd2fdbdb47572549
Jochem Bloem <j.bloem1@uu.nl>
2021-05-24 14:05:19 +0200

    Added a new file 'termination', which will contain all methods pertaining to program termination

---
+++ src/termination.h
@@ -0,0 +1,22 @@
+/*
+This program has been developed by students from the bachelor Computer Science at
+Utrecht University within the Software Project course.
+© Copyright Utrecht University (Department of Information and Computing Sciences)
+*/
+
+
+#pragma once
+namespace termination
+{
+	void successful();
+
+	void failureCrawler();
+
+	void failureSpider();
+
+	void failureParser();
+
+	void failureDatabase();
+
+	void failure();
+};

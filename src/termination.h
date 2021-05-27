a26b1525d5515f1c56052202232f1aba1a724698
Jochem Bloem <45417884+JochemBloem@users.noreply.github.com>
2021-05-27 12:50:07 +0200

    Summaries

--- src/termination.h
+++ src/termination.h
@@ -8,15 +8,33 @@ Utrecht University within the Software Project course.
 #pragma once
 namespace termination
 {
+	/// <summary>
+	/// Terminates the program with status code 0 (no errors).
+	/// </summary>
 	void successful();
 
+	/// <summary>
+	/// Terminates the program with status code 1 (failure).
+	/// </summary>
 	void failure();
 
+	/// <summary>
+	/// Handles retrying functionality when the Crawler failed, and terminates when we ran into a fatal failure.
+	/// </summary>
 	void failureCrawler(const char* file, int line);
 
+	/// <summary>
+	/// Handles retrying functionality when the Spider failed, and terminates when we ran into a fatal failure.
+	/// </summary>
 	void failureSpider(const char* file, int line);
 
+	/// <summary>
+	/// Handles retrying functionality when the Parser failed, and terminates when we ran into a fatal failure.
+	/// </summary>
 	void failureParser(const char* file, int line);
 
+	/// <summary>
+	/// Handles retrying functionality when the Database (API) failed, and terminates when we ran into a fatal failure.
+	/// </summary>
 	void failureDatabase(const char* file, int line);
 };

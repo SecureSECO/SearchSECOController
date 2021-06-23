96c15a31903cab822eab67f24d6a77df93ca2adf
ilan hartskeerl <ilan@hartskeerl.nl>
2021-06-23 14:13:38 +0200

    Fixed copyright symbols

--- libSearchSECOController/termination.h
+++ libSearchSECOController/termination.h
@@ -1,7 +1,7 @@
 /*
 This program has been developed by students from the bachelor Computer Science at
 Utrecht University within the Software Project course.
-� Copyright Utrecht University (Department of Information and Computing Sciences)
+© Copyright Utrecht University (Department of Information and Computing Sciences)
 */
 
 #pragma once
